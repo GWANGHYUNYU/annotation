24b2abf1a4d506a875fa5bb4f07107b458ff5ce0
Ka0Ri <dtvu1707@gmail.com>
2024-08-02 17:34:23 +0900

    change window name

--- main.py
+++ main.py
@@ -356,6 +356,6 @@ class MainWindowClass(QMainWindow, form_class):
 # 프로그램 시작(QT application)
 app = QApplication(sys.argv)
 w = MainWindowClass()
-w.setWindowTitle('WeedCroppingManagerNet')
+w.setWindowTitle('Face Detection Annotation Tool made by AISEED')
 w.show()
 app.exec_()
